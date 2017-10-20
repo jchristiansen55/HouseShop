63444abf0748695662cacd3aa958691585bee877
fhboswell <franklin.henry.boswell@gmail.com>
2017-10-20 14:14:49 -0700

    double on one page

--- routes/search.js
+++ routes/search.js
@@ -18,4 +18,19 @@ router.post('/', function(req, res, next) {
 });
 
 
+router.post('/other', function(req, res, next) {
+    //res.sendFile(path.join(__dirname + '/index.html'));
+    models.Listing.findAll({
+  		where: {
+    		city: req.body.city
+  		}
+	}).then(function(listings) {
+        res.render('search', {
+            title: 'Listings',
+            listings: listings
+        });
+    });
+});
+
+
 module.exports = router;
