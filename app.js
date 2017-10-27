d6a766e3b12aab100e7363837efd47040de7e7fc
Kevin Lay <kevinlay689@yahoo.com>
2017-10-27 13:48:09 -0700

    Added filter and show all functionality

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var search = require('./routes/search');
+var filter = require('./routes/filter');
 var models = require('./models');
 var app = express();
 
@@ -53,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/search', search);
+app.use('/filter', filter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
