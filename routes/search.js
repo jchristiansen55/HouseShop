5f5fb008ebd04fec8a12c227da6d9dc119453c20
nemimccarter <nemimccarter@gmail.com>
2017-10-28 14:17:17 -0700

    Remove changes that resulted in losing node modules

--- routes/search.js
+++ routes/search.js
@@ -14,22 +14,22 @@ router.post('/', function(req, res, next) {
             [Op.or]: [
                 {
                     address: {
-                        [Op.like]: '%' + req.body.searchQuery + '%'
+                        [Op.like]: '%' + req.body.city + '%'
                     }
                 },
                 {
     		        city: {
-                        [Op.like]: '%' + req.body.searchQuery + '%'
+                        [Op.like]: '%' + req.body.city + '%'
                     }
                 },
                 {
                     state: {
-                        [Op.like]: '%' + req.body.searchQuery + '%'
+                        [Op.like]: '%' + req.body.city + '%'
                     }
                 },
                 {
                     zipcode: {
-                        [Op.like]: '%' + req.body.searchQuery + '%'
+                        [Op.like]: '%' + req.body.city + '%'
                     }
                 }
             ]
@@ -52,6 +52,22 @@ router.get('/', function(req, res, next) {
         });
     });
 });
+
+/*
+router.post('/other', function(req, res, next) {
+    //res.sendFile(path.join(__dirname + '/index.html'));
+    models.Listing.findAll({
+  		where: {
+    		city: req.body.city
+  		}
+	}).then(function(listings) {
+        res.render('search', {
+            title: 'Listings',
+            listings: listings
+        });
+    });
+});
+*/
 
 
 module.exports = router;
