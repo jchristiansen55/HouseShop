65eb1a14c5e46860e06b6608ceaf76d8c0d45615
nemimccarter <nemimccarter@gmail.com>
2017-11-06 20:02:58 -0800

    refactor. remove unused code

--- config/local-strategy.js
+++ config/local-strategy.js
@@ -6,17 +6,19 @@ passport.use(new LocalStrategy( {
     usernameField: 'email',
     passwordField: 'password',
     session: false
-    }, function(email, password, done) {
-        console.log("email: " + email + " password: " + password);
-    models.User.findOne({ where: {email: email, password: password}}).then(user => {
-/*      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
-      */
-  passport.serializeUser(function(user, done) {
-      console.log("in passport.js");
-    done(null, user.id);
-  });
+    }, function(email, password, done, err) {
+        models.User.findOne({ where: {email: email, password: password}}).then(user => {
+            if (err) { 
+                return done(err); 
+            }
+            
+            if (!user) { 
+                return done(null, false); 
+            }
+      
+            passport.serializeUser(function(user, done) {
+                done(null, user.id);
+            });
 
 
       return done(null, user);
