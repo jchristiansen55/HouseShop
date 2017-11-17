c9daad487495e01197aff8ba1ab81307e7ee8a39
khanhngg <khanhnguye94@gmail.com>
2017-11-17 15:23:21 -0800

    carousel working with the passed listingid variable

--- routes/listing.js
+++ routes/listing.js
@@ -8,17 +8,20 @@ router.get('/:listing?', function(req, res, next) {
     //res.sendFile(path.join(__dirname + '/index.html'));
     var listingid = req.query.listing;
     console.log(listingid);
+
     models.Listing.findOne({where: {id: listingid}})
     .then(function(listing) {
-        res.render('listing', { // render the Listing page
-            title: 'Listing - GET', // remove 'GET'
-            listing: listing,
-        });
+        models.Media.findAll({where: {listingid: listingid}})
+        .then(function(media) {
+            res.render('listing', { // render the Listing page
+                title: 'Listing - GET', // remove 'GET'
+                listing: listing,
+                media: media
+            });    
+        });  
+        
     });
-    models.Media.findAll({where: {listingid: listingid}})
-    .then(function(media) {
-        media: media
-    });
+
 });
 
 module.exports = router;
