0e08a1d23d5200daf30229a498f284200402f462
nemimccarter <nemimccarter@gmail.com>
2017-12-15 15:18:02 -0800

    Correct path for server

--- routes/listing.js
+++ routes/listing.js
@@ -23,8 +23,8 @@ router.get('/:listing?', function(req, res, next) {
                     media: media,
                     user: user, // listing agent
                     errors: [],
-                    UserState: req.cookies.UserState
-                    // User: req.cookies.User
+                    UserState: req.cookies.UserState,
+                    User: req.cookies.User
                 });
             });
         });
