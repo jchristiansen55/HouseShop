c208f1f4985a40c5143805bee3fb036eb4044d44
Eugene Stukalov <estukalov@paypal.com>
2017-12-17 22:48:06 -0800

    send all message senders to ejs as a map

--- routes/messageBoard.js
+++ routes/messageBoard.js
@@ -12,16 +12,51 @@ router.get('/', function(req, res, next) {
     models.Message.findAll({
         where: {
             receiver: userId
-        } 
+        }
     })
     .then(function(messages) {
-        res.render('messageBoard', {
-            title: "Your Inbox",
-            messages: messages,
-            UserState: req.cookies.UserState,
-            User: req.cookies.User,
+        var senderIds = [];
+        messages.forEach(function(message) {
+            senderIds.push(message.sender);
+        });
+        models.User.findAll({
+
+                where: {
+                    id: {
+                        [Op.in]: senderIds
+                    }
+                }
+        }).then(function(senders) {
+            var senderToUser = new Map();
+
+
+            messages.forEach(function(message) {
+                var senderObject = getSenderObject(message, senders);
+                console.log("senderObject f n " + senderObject.firstName);
+                senderToUser.set(message.sender, senderObject);
+                console.log("message.sender " + message.sender);
+
+            });
+            res.render('messageBoard', {
+                title: "Your Inbox",
+                messages: messages,
+                senderToUser: senderToUser,
+                UserState: req.cookies.UserState,
+                User: req.cookies.User,
+            });
         });
     });
 });
+
+function getSenderObject(message, senders) {
+    var sender;
+    senders.forEach(function(thisSender) {
+        if (message.sender == thisSender.id) {
+            console.log("found sender " + thisSender);
+            sender = thisSender;
+        }
+    });
+    return sender;
+}
 
 module.exports = router;
